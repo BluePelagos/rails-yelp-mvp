dbabe181325a4bc22f9a8af4eeddd5e4c700864d
Leo Chan <lchan168@hotmail.com>
2019-05-22 14:49:51 +0800

    completed, no views yet

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(reviews_param)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+
+    if @review.save
+      redirect_to restaurant_path(params[:restaurant_id])
+    else
+      render :new
+    end
+  end
+
+  def reviews_param
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# Rails.application.routes.draw do
+#   get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: 'new_review'
+#   post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: 'create_review'
+#   get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+#   post 'restaurants', to: 'restaurants#create', as: 'create_restaurant'
+#   get 'restaurants', to: 'restaurants#index', as: 'restaurants'
+#   get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
