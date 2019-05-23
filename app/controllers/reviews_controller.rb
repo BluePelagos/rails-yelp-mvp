64be4377826a511f436c63a487f487ab8fc83f60
Leo Chan <lchan168@hotmail.com>
2019-05-23 09:07:40 +0800

    updated simple_forms for no error issue

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,12 @@ class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
-    # raise
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
 
     if @review.save
       redirect_to restaurant_path(@review.restaurant)
